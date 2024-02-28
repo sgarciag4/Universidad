0ee3786ce0f91e892095407a01ff425557e3a10b
Sebas G <sgarciag4@ucentral.edu.co>
2024-02-28 16:33:40 -0500

    version 4

--- src/main/java/co/vinni/soapproyectobase/SoapProyectobaseApplication.java
+++ src/main/java/co/vinni/soapproyectobase/SoapProyectobaseApplication.java
@@ -5,8 +5,10 @@ import co.vinni.soapproyectobase.entidades.Vehiculo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * author Vinni 2023
@@ -16,18 +18,38 @@ public class SoapProyectobaseApplication {
 
     public static void main(String[] args) {
 
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        Scanner datos = new Scanner (System.in);
         ControladorVehiculo cv = new ControladorVehiculo();
         Vehiculo vehiculo = new Vehiculo();
         Vehiculo vehiculo2 = new Vehiculo();
         Date fecha = new Date(124, 1, 27, 10, 30, 15);
         SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-        vehiculo.setPais("Ecuador");
-        vehiculo.setPrecio(70000);
-        vehiculo.setTipo("camioneta");
-        vehiculo.setMunicipio("Bogota");
+       // vehiculo.setPais("Ecuador");
+        //   vehiculo.setPrecio(70000);
+        //  vehiculo.setTipo("camioneta");
+      //  vehiculo.setMunicipio("Bogota");
         vehiculo.setFechaAdquisicion(fecha);
-        vehiculo.setMarca("audi");
-        vehiculo.setDepartamento("Bogota");
+       // vehiculo.setMarca("audi");
+       // vehiculo.setDepartamento("Bogota");
+
+        System.out.println("Bienvenido");
+
+        System.out.println("Digite  el país:");
+        vehiculo.setPais(datos.next());
+        System.out.println("Digite  el Departamento:");
+        vehiculo.setDepartamento(datos.next());
+        System.out.println("Digite  el Municipio:");
+        vehiculo.setMunicipio(datos.next());
+//        System.out.println("Digite  fecha adquisición seguro:");
+//        vehiculo.setFechaAdquisicion(datos());
+        System.out.println("Digite  precio comercial del vehiculo:");
+        vehiculo.setPrecio(datos.nextDouble());
+        System.out.println("Digite  el tipo del vehiculo:");
+        vehiculo.setTipo(datos.next());
+        System.out.println("Digite  la marca del vehiculo:");
+        vehiculo.setMarca(datos.next());
+
 
 //        vehiculo2.setPais("Ecuador");
 //        vehiculo2.setPrecio(70000);
@@ -50,109 +72,6 @@ public class SoapProyectobaseApplication {
                 System.out.println(obj);
             }
         }
-
-
-
-
-        //SpringApplication.run(SoapProyectobaseApplication.class, args);
-//        ControladorCuenta controlaCuenta = new ControladorCuenta();
-//        Cuenta cuenta = new Cuenta();
-//        cuenta.setNombre("pruebaCliente2");
-//        cuenta.setApellidos("Transferencia2");
-//        cuenta.setNumerodeCedula("10302");
-//        cuenta.setTipoDocumento("cc");
-//        cuenta.setSaldo(0);
-//        cuenta.setActiva(true);
-//        cuenta.setNumerodeCelular("32122212");
-//        if(controlaCuenta.registrarCuenta(cuenta)){
-//            System.out.printf("Usuario Registrado con Exito");
-//        }else{
-//            System.out.println("Usuario Existente");
-//        }
-
-        //hacer una consignacion por numero de cuenta
-
-//        ControladorCuenta controlaCuenta = new ControladorCuenta();
-//        if(!controlaCuenta.Consignacion(20000,133000,796123845)){
-//            System.out.println("Consignacion invalida");
-//        }
-//        System.out.printf("Consigacion existosa");
-
-
-        //hacer una consignacion por numero
-//        ControladorCuenta controlaCuenta = new ControladorCuenta();
-//        controlaCuenta.transferencia(3000,"320122321");
-
-
-
-//        ControladorCuenta controlaCuenta = new ControladorCuenta();
-//        ControladorCuenta controladorCuenta = new ControladorCuenta();
-//        controladorCuenta.leerCuenta("Clientes");
-//        controladorCuenta.leerCuenta("datoCuenta265439871");
-
-
-        //ver estado de cuenta;
-//        ControladorCuenta controladorCuenta = new ControladorCuenta();
-//
-//        cuenta = controladorCuenta.estadoCuenta("10301","346187295");
-//        System.out.println(cuenta);
-//        System.out.println("adasdas");
-//        Scanner lectura = new Scanner (System.in);
-//        ControladorCuenta controlaCuenta = new ControladorCuenta();
-//        Cuenta cuenta = new Cuenta();
-//        boolean salir = false;
-//        while(!salir){
-//            System.out.println("***Bienvenido a Cuenta***");
-//            System.out.println("Marque una opción");
-//            System.out.println("1. Creacion de cuenta");
-//            System.out.println("2. Verificar estado de Cuenta ya existente");
-//            System.out.println("3. Realizar una Consignacion a una Cuenta");
-//            System.out.println("4. Realizar una Transferencia a un Numero Celular");
-//            System.out.println("5. Salir");
-//            int opc = lectura.nextInt();
-//            switch (opc){
-//                case 1:
-//                    System.out.println("Digite su Nombre");
-//                    cuenta.setNombre(lectura.next());
-//                    System.out.println("Digite su Apellido");
-//                    cuenta.setApellidos(lectura.next());
-//                    System.out.println("Digite su Numero de identificacion");
-//                    cuenta.setNumerodeCedula(lectura.next());
-//                    System.out.println("Digite su tipo de documento");
-//                    cuenta.setTipoDocumento(lectura.next());
-//                    cuenta.setSaldo(0);
-//                    cuenta.setActiva(true);
-//                    System.out.println("Digite su Numero Celular");
-//                    cuenta.setNumerodeCelular(lectura.next());
-//                    if(controlaCuenta.registrarCuenta(cuenta)){
-//                        System.out.printf("Gracias por Registrarse");
-//                    }else{
-//                        System.out.println("Usuario Existente");
-//                   }
-//                    break;
-//                case 2:
-//                    System.out.println("Digite el numero de cedula");
-//                    String numeroCedula = lectura.next();
-//                    System.out.println("Digite el numero de cuenta");
-//                    String numeroCuenta = lectura.next();
-//                    cuenta = controlaCuenta.estadoCuenta(numeroCedula,numeroCuenta);
-//                    System.out.println(cuenta);
-//                    break;
-//                case 3:
-//                    System.out.println();
-//
-//                if(!controlaCuenta.Consignacion(20000,133000,796123845)){
-//                    System.out.println("Consignacion invalida");
-//                }
-//                System.out.printf("Consigacion existosa");
-//                    case
-//            }
-//
-//        }
-
-
-
-
     }
 
 }
